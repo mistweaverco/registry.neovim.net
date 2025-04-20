39eed031bba0ec95601db314300434893c83dee6
Marco Kellershoff <marco@kellershoff.net>
2025-04-20 13:25:06 +0200

    fix(ci): release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ GH_TAG=$(date +%Y-%m-%d-%H-%M)
 
 create_registry() {
   (cd scripts && bun . &&  zip -r ../neovim-registry.json.zip ../web/static/neovim-registry.json) || exit 1
-  sha256sum ../web/static/neovim-registry.json ../neovim-registry.json.zip > ../neovim-registry-checksums.txt || exit 1
+  sha256sum web/static/neovim-registry.json neovim-registry.json.zip > neovim-registry-checksums.txt || exit 1
 }
 
 set_release_action() {
@@ -22,10 +22,10 @@ set_release_action() {
 do_gh_release() {
   if [ "$RELEASE_ACTION" == "edit" ]; then
     echo "Overwriting existing release $GH_TAG"
-    gh release upload --clobber "$GH_TAG" ../neovim-registry.json.zip ../neovim-registry-checksums.txt
+    gh release upload --clobber "$GH_TAG" neovim-registry.json.zip neovim-registry-checksums.txt
   else
     echo "Creating new release $GH_TAG"
-    gh release create --generate-notes "$GH_TAG" ../neovim-registry.json.zip ../neovim-registry-checksums.txt
+    gh release create --generate-notes "$GH_TAG" neovim-registry.json.zip neovim-registry-checksums.txt
   fi
 }
 
